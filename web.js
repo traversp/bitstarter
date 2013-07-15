f3090c178b7238f17d1cd82e489028cfcb86b6c0
Ubuntu <ubuntu@ip-10-154-174-140.ec2.internal>
2013-07-15 17:20:48 +0000

    Updated fsread web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer(16);
+buffer = fs.readFileSync(index.html);
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString('utf-8', 0, 12));
 });
 
 var port = process.env.PORT || 5000;
